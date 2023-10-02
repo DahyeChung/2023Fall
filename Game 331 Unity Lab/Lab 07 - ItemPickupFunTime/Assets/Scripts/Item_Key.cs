ed41624eba05494b80257d757a17067ef56954fc
Dahye Chung <lewachung@gmail.com>
2023-10-02 11:47:18 +0900

    331 : Lab07 완료

--- Game 331 Unity Lab/Lab 07 - ItemPickupFunTime/Assets/Scripts/Item_Key.cs
+++ Game 331 Unity Lab/Lab 07 - ItemPickupFunTime/Assets/Scripts/Item_Key.cs
@@ -7,14 +7,14 @@ public class Item_Key : ItemBase
 
     protected override void OnItemUsed(PlayerActionScript playerActionScript)
     {
-        Door[] doors = FindObjectsOfType<Door>();
+        Door[] doors = FindObjectsOfType<Door>(); //모든 문찾기
         foreach(Door door in doors)
         {
             Vector3 vectorToPlayer = playerActionScript.gameObject.transform.position - door.transform.position;
             float distanceToPlayer = vectorToPlayer.magnitude;
             if (distanceToPlayer < 2.0f)
             {
-                Destroy(door.gameObject);
+                Destroy(door.gameObject); //문을 찾으면 Destory
             }
         }
     }
---
+++ Game 331 Unity Lab/Lab 07 - ItemPickupFunTime/Assets/Scripts/Item_Mushroom.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item_Mushroom : ItemBase
+{
+
+    protected override void OnItemUsed(PlayerActionScript playerActionScript)
+    {
+        playerActionScript.transform.localScale *= 2.0f;
+    }
+}
